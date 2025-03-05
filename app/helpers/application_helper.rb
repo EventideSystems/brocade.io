34e0994d9930c14e0688551af1408d942d27c5ad
Tom Tuddenham <ferrisoxide@gmail.com>
2025-03-06 10:12:08 +1030

    119 basic support for dark mode

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,19 @@
 # frozen_string_literal: true
 
+# Application helper
 module ApplicationHelper
   include Pagy::Frontend
+
+  DESKTOP_LINK_TO_CLASS = 'font-medium text-gray-500 dark:text-white hover:text-gray-900 dark:hover:text-gray-400'
+  MOBILE_LINK_TO_CLASS = 'block rounded-md px-3 py-2 text-base font-medium text-gray-700 dark:text-white hover:bg-gray-50 hover:text-gray-900 dark:hover:text-gray-400' # rubocop:disable Layout/LineLength
+
+  def desktop_link_to(*, **options)
+    options[:class] = DESKTOP_LINK_TO_CLASS
+    link_to(*, **options)
+  end
+
+  def mobile_link_to(*, **options)
+    options[:class] = MOBILE_LINK_TO_CLASS
+    link_to(*, **options)
+  end
 end
