9977d8a8ec02d90bad62dd2c601a69e7b7497e0f
Tom Tuddenham <ferrisoxide@gmail.com>
2025-05-22 21:45:51 +0930

    132 added basic support for name/brand search

--- app/controllers/concerns/product_search.rb
+++ app/controllers/concerns/product_search.rb
@@ -10,6 +10,8 @@ module ProductSearch # rubocop:disable Style/Documentation
   def product_search(params)
     if params[:query].present?
       pagy, records = pagy(build_query(params[:query]))
+    elsif params[:name].present? || params[:brand].present?
+      pagy, records = pagy(build_param_query(params))
     else
       pagy, records = pagy(Product.all)
     end
@@ -25,6 +27,14 @@ module ProductSearch # rubocop:disable Style/Documentation
     return base_query unless gtin?(query_params)
 
     base_query.or(Product.by_gtin(query_params))
+  end
+
+  def build_param_query(params)
+    base_query = Product.all
+    base_query = base_query.where('name ILIKE :name', name: "%#{params[:name]}%") if params[:name].present?
+    base_query = base_query.where('brand_name ILIKE :brand', brand: "%#{params[:brand]}%") if params[:brand].present?
+
+    base_query
   end
 
   def gtin?(string)
